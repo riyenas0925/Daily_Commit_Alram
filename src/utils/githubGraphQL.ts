ae6b7b18e18ddcbbfc55e1f0fc2c04aed077e3ca
Dongmin Kang <riyenas0925@gmail.com>
2020-07-09 23:25:15 +0900

    ENH : Github Action 기능 추가, AccessToken 변경(Public Token)

--- src/utils/githubGraphQL.ts
+++ src/utils/githubGraphQL.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const accessToken: string = '2f4ef526a1585e50c289611defff848b1fcd6e80'
+const accessToken: string = '87a3f7a3e7ae630dd5e199aa7f2cdc88a47e8946'
 
 export const githubGraphQL = async(query: string) => {
     const response = await axios.post("https://api.github.com/graphql", {
