b612a9fc2946b3291c09b7e3fed8ce201a05d136
riyenas0925 <riyenas0925@gmail.com>
2020-07-09 23:02:10 +0900

    ENH : axios를 이용한 github GraphQL API에서 특정날짜 이후 리포지토리별로 커밋한  커밋메시지 개수를 반환 하는 기능 구현
    Resolves #3

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello Rieyans')
+import {githubGraphQL} from './utils/githubGraphQL'
+
+const userId: string = 'riyenas0925';
+const todayDate: string = '2020-07-04T00:00:00Z';
+const query: string = `
+query {
+    user(login: "${userId}") {
+        repositories(last: 100, orderBy: {field: PUSHED_AT, direction: DESC}) {
+            totalCount
+            nodes {
+                name
+                refs(first: 100, refPrefix: "refs/heads/", after: "") {
+                    edges {
+                        node {
+                            name
+                            target {
+                                ... on Commit {
+                                    history(since: "${todayDate}") {
+                                        totalCount
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+`;
+
+console.log(githubGraphQL(query));
