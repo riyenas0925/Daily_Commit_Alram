94267d468bc264fcea34b465184cf8504aa8f63c
riyenas0925 <riyenas0925@gmail.com>
2020-07-12 23:15:27 +0900

    ENH : Github GraphQL 응답에서 사용자 이메일, id 얻기
    Resolves #15

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import {githubGraphQL} from './graphQL/githubGraphQL'
 import {startDate} from './utils/timeStamp'
 import {todayCommitCount, userDetails} from './graphQL/query'
+import {userParser} from './parser/user'
 
 (async () => {
     const userId: any = process.env.USER_ID;
     const since: string = startDate().toISOString();
 
-    const response1 = await githubGraphQL(userDetails());
-    console.log(response1);
+    const userDetailsResponse = await githubGraphQL(userDetails());
+    const user = userParser(userDetailsResponse);
 
-    const response2 = await githubGraphQL(todayCommitCount(userId, since));
-    console.log(response2);
+    console.log('user : ' + user.id + ', email : ' + user.email);
+
+    const todayCommitCountRes = await githubGraphQL(todayCommitCount(userId, since));
+    console.log(todayCommitCountRes);
 })().catch(e => {
     console.error(e);
 });
