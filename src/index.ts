5df8c952f266b622322b0c3b6ac7b53097eb6562
riyenas0925 <riyenas0925@gmail.com>
2020-07-15 23:30:37 +0900

    ENH : nodemailer를 이용해 gmail로 일일 커밋 현황 받기

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-import {githubGraphQL} from './graphQL/githubGraphQL'
-import {startDate} from './utils/timeStamp'
-import {todayCommitCount, userDetails} from './graphQL/query'
-import {userParser} from './parser/user'
-import {todayCommitParser} from './parser/todayCommit'
+import { githubGraphQL } from './graphQL/githubGraphQL'
+import { startDate } from './utils/timeStamp'
+import { todayCommitCount } from './graphQL/query'
+import { todayCommitParser } from './parser/todayCommit'
+import { sendEmail } from './utils/sendEmail'
 
 (async () => {
     const userId: any = process.env.USER_ID;
     const since: string = startDate().toISOString();
-    const user = userParser(await githubGraphQL(userDetails()));
-    console.log(user);
-
+    
     const todayCommit = todayCommitParser(await githubGraphQL(todayCommitCount(userId, since)));
     console.log(todayCommit);
+
+    const info = await sendEmail(since , todayCommit.toString());
+    console.log(info);
 })().catch(e => {
     console.error(e);
 });
